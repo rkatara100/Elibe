04904880de53dd11b3c75439960f25ce4d146715
rkatara100 <rkatara100@gmail.com>
2025-02-20 17:35:35 +0530

    add  config

--- server.ts
+++ server.ts
@@ -1,1 +1,15 @@
-console.log("welcome to apis");
+import app from "./src/app";
+
+import { config } from "./src/config/config";
+
+const startServer=()=>{
+      const port=config.port||3000;
+
+      app.listen(port,()=>{
+            console.log(`listening on PORT ${port}`);
+      })
+};
+
+
+
+startServer();
