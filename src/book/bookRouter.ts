5a26ab5815c36a1d83952a2955dd47f676f0f5f1
rkatara100 <rkatara100@gmail.com>
2025-02-25 20:28:09 +0530

    add jwt verification

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -3,13 +3,14 @@ const bookRouter=express.Router();
 import {createBook} from './bookController';
 import multer from "multer";
 import path from "node:path";
+import authenticate from "../middlewares/authenticate";
 
 const upload=multer({
       dest:path.resolve(__dirname,'../../public/data/uploads'),
       limits:{fileSize:1.05e7}//~29MB
 })
 
-bookRouter.post('/create',upload.fields([
+bookRouter.post('/create',authenticate,upload.fields([
      { name:'coverImage',maxCount:1},
      {name:'file',maxCount:1}
 
